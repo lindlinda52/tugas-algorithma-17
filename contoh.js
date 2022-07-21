b3eb4a61c499e994fa46182b6860a8b9d16202f1
lindlinda52 <109120061+lindlinda52@users.noreply.github.com>
2022-07-21 23:53:42 +0800

    Add files via upload

---
+++ contoh.js
@@ -0,0 +1,38 @@
+//Tugas Algoritma PPT
+//proyek 1
+let jam = 2
+let detik = 3600
+
+let waktu = jam * detik
+console.log(waktu);
+
+
+//proyek 2
+
+let nilai1 = 10
+let nilai2 = 20
+if (nilai1 > nilai2) {
+  console.log(nilai1)
+}else if(nilai1 === nilai2){
+  console.log(nilai1 || nilai2)
+}else{
+  console.log(nilai2)
+}
+
+
+//proyek 3
+
+let jumlahTreveler = 2
+let hargaTiket = 600.000
+let totalHarga
+totalHarga = jumlahTreveler * hargaTiket
+console.log(totalHarga)
+
+//proyek 4
+
+let angka = 100
+
+for(angka = 1; angka <= 100; angka++)
+  console.log(angka);
+
+
